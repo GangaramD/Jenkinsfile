a996ae18fb9589fa023651c628631ebd78ca88a4
GangaramD <gangaram136@gmail.com>
2019-12-16 19:45:23 +0530

    Create nexus.sh

---
+++ project2/nexus.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+user=$nexususer
+password=$pass
+repo_url="$nexusrepo"    #nexus repo url
+Repo_name="Cropin"
+sourcerepofolder="$sourcerepofolder"
+name=$Appname
+version=$Appversion
+date=`echo $version | awk -F . '{print $2}'`
+#artifactid=$2
+filename="$name-$version"
+groupid=$sourcerepofolder/$date/products/webservices
+artifactid=$name
+
+echo "Script to donwload artifacts from the nexus repo"
+wget --user=$user --password=$password http://$nexusrepourl/repository/$Repo_name/$groupid/$artifactid/$version/$filename.msi	
+
+export filename=$filename
+echo "$filename"
